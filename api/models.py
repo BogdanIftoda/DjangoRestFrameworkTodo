c1f2ff6d5b7629e335b1d7bb381a4f84aa6ffa88
BogdanIftoda <bogdaniftoda@gmail.com>
2021-02-10 15:28:27 +0200

    add readme

--- api/models.py
+++ api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Task(models.Model):
     title = models.CharField(max_length=50)
+    created = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.title
